494a7630d370cda614dcaef0748ffb6fe33b11bc
uym2 <ceciliamaitouyen@gmail.com>
2016-12-07 18:58:48 -0800

    update br_len_stats.py

--- br_len_stats.py
+++ br_len_stats.py
@@ -10,15 +10,13 @@ pattern = re.compile(r"\:[0-9]+(?:\.[0-9]+)?")
 with open(filename,'r') as f:
 	tree = f.readlines()
 
-#print pattern.findall(str[0])
 br_len = [float(x[1:]) for x in pattern.findall(tree[0])]
 
 br_count = len(br_len)
-br_avg = sum(br_len)/br_count
-
-#print br_count
-#print br_avg
+br_sum = sum(br_len)
+br_avg = br_sum/br_count
 
 print "branch #: " + str(br_count)
+print "branch sum: " + str(br_sum)
 print "branch avg: " + str(br_avg)
 
