ee0e2ed7bc7d4463371db87f4f8d91da8a16e202
uym2 <ceciliamaitouyen@gmail.com>
2016-12-11 23:58:22 -0800

    fixed cmpTrees.truepos.sh; added supportive functions in numlist

--- cmpTrees.truepos.sh
+++ cmpTrees.truepos.sh
@@ -3,4 +3,4 @@
 refTree=$1
 cmpTree=$2
 
-compareTrees.leaf_include $refTree $cmpTree | awk -F '\t' '{print $1,$3}' | tail -n +2 | numlist -fill 
+compareTrees.leaf_include $refTree $cmpTree | awk -F '\t' '{print $1,$3}' | tail -n +2 | numlist -rmblk 
