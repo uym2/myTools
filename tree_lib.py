6ab1ca8e08ae5db11f4815e05d36cfbf8b160a16
uym2 <ceciliamaitouyen@gmail.com>
2017-03-13 16:03:26 -0700

    write_newick for tree_lib

--- tree_lib.py
+++ tree_lib.py
@@ -1,3 +1,4 @@
+import sys
 from dendropy import Tree
 
 def get_taxa(tree_file,scheme='newick'):
@@ -10,6 +11,41 @@ def report_taxa(tree_file,scheme='newick',listing=True,counting=True):
 	a_tree.read_from_path(tree_file,scheme)
 	if listing:
 		for leaf in a_tree.leaf_nodes():
-			print leaf.taxon.label
+			print(leaf.taxon.label)
 	if counting:
-		print 'Taxa #: ' + str(len(a_tree.leaf_nodes()))
+		print('Taxa #: ' + str(len(a_tree.leaf_nodes())))
+
+def tree_as_newick(a_tree,outfile=None,append=False):
+# dendropy's method to write newick seems to have problem ...
+	if outfile:
+		outstream = open(outfile,'a') if append else open(outfile,'w')
+	else:
+		outstream = sys.stdout
+
+	__write_newick(a_tree.seed_node,outstream)
+
+	outstream.write(";\n")
+	if outfile:
+		outstream.close()	
+
+def __write_newick(node,outstream):
+	if node.is_leaf():
+			if node.taxon.label:
+				outstream.write(node.taxon.label)
+			else:
+				outstream.write(str(node.label))
+	else:
+		outstream.write('(')
+		is_first_child = True
+		for child in node.child_node_iter():
+			if is_first_child:
+				is_first_child = False
+			else:
+				outstream.write(',')
+			__write_newick(child,outstream)
+		outstream.write(')')
+	if not node.is_leaf() and node.label is not None:
+			outstream.write(str(node.label))
+
+	if not node.edge_length is None:
+		outstream.write(":"+str(node.edge_length))
