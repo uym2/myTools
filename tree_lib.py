d200c915268acd9307a94a347b472cd1fe334891
uym2 <ceciliamaitouyen@gmail.com>
2017-03-13 18:39:23 -0700

    issue with dendropy version

--- tree_lib.py
+++ tree_lib.py
@@ -3,7 +3,7 @@ from dendropy import Tree
 
 def get_taxa(tree_file,scheme='newick'):
 	a_tree = Tree()
-	a_tree.read_from_path(tree_file,scheme)
+	a_tree.get_from_path(tree_file,scheme)
 	return [leaf.taxon.label for leaf in a_tree.leaf_nodes()]
 
 def report_taxa(tree_file,scheme='newick',listing=True,counting=True):
