df87d28eb9baf2edd1c81fef12eecbdc7a4c2909
uym2 <ceciliamaitouyen@gmail.com>
2017-04-20 13:51:36 -0700

    update job-running scripts on comet

--- split_jobs.sh
+++ split_jobs.sh
@@ -3,7 +3,7 @@
 taskfile=$1
 nj=$2 # number of jobs per splitted taskfile
 np=$3 # number of processors per job
-machine=$4 # comet/gordon
+ncore=$4 # number of cores per node
 jobname=$5
 queue=$6
 
@@ -19,10 +19,11 @@ while read line; do
 	else
 		mkdir job\_$i
 		mv $curr_file job\_$i/task$i
-		cp /home/umai/my_gits/myTools/jobscr_template.$machine job\_$i/jobscr_$i.$machine
-		sed -i "s/#####/$jobname/g" job\_$i/jobscr_$i.$machine
-		sed -i "s/%%%%%/$queue/g" job\_$i/jobscr_$i.$machine
-		echo python /home/umai/my_gits/myTools/core_launcher.py task$i $np >> job\_$i/jobscr_$i.$machine
+		cp /home/umai/my_gits/myTools/jobscr_template.comet job\_$i/jobscr_$i.comet
+		sed -i "s/#####/$jobname/g" job\_$i/jobscr_$i.comet
+		sed -i "s/%%%%%/$queue/g" job\_$i/jobscr_$i.comet
+		sed -i "s/@@@@@/$ncore/g" job\_$i/jobscr_$i.comet
+		echo python /home/umai/my_gits/myTools/core_launcher.py task$i $np >> job\_$i/jobscr_$i.comet
 		j=1
 		i=$((i+1))
 		curr_file=`mktemp task-XXXXX`
@@ -32,8 +33,11 @@ done < $taskfile
 if [ -s $curr_file ]; then
 	mkdir job\_$i
 	mv $curr_file job\_$i/task$i
-	cp jobscr_template.$machine job\_$i/jobscr_$i.$machine
-	echo python `pwd`/core_launcher.py task$i $np >> job\_$i/jobscr_$i.$machine
+	cp /home/umai/my_gits/myTools/jobscr_template.comet job\_$i/jobscr_$i.comet
+	sed -i "s/#####/$jobname/g" job\_$i/jobscr_$i.comet
+	sed -i "s/%%%%%/$queue/g" job\_$i/jobscr_$i.comet
+	sed -i "s/*****/$ncore/g" job\_$i/jobscr_$i.comet
+	echo python /home/umai/my_gits/myTools/core_launcher.py task$i $np >> job\_$i/jobscr_$i.comet
 else
 	rm $curr_file
 fi
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,5 +1,4 @@
 from=$1
 to=$2
-device=$3
-for i in `seq $from $to`; do cd job_$i; sbatch jobscr_$i\.$device; cd ..; done
+for i in `seq $from $to`; do cd job_$i; sbatch jobscr_$i.comet; cd ..; done
 
