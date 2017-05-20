9d66f40449ea29f2947042e92ed102433c0968a9
uym2 <ceciliamaitouyen@gmail.com>
2017-05-19 21:53:40 -0700

    add remove_taxon_from_tree.py

---
+++ remove_taxon_from_tree.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+'''
+Created on Jun 3, 2011
+
+@author: smirarab
+'''
+import dendropy
+import sys
+import os
+import copy
+import os.path
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 3: 
+        print("USAGE: treefile species_list_file [output]")
+        sys.exit(1)
+    treeName = sys.argv[1]
+    #sample = open(sys.argv[2])
+    included = [s for s in sys.argv[3:]]
+    resultsFile="%s.%s" % (treeName, "renamed")
+    trees = dendropy.TreeList.get_from_path(treeName, 'newick',preserve_underscores=True)
+    filt = lambda node: True if (node.taxon is not None and node.taxon.label in included) else False
+    for tree in trees:
+        tree.filter_leaf_nodes(filt)
+        #tree.deroot()
+        #tree.reroot_at_midpoint(update_splits=False)
+    trees.write(file=open(sys.argv[2],'w'), schema='newick', suppress_rooting=True)
