7979b3fbc934016bd320b34d3bcfa091ef85736c
Uyen To Mai <umai@tscc-login2.sdsc.edu>
2017-07-10 16:00:33 -0700

    updates to include tscc

--- split_jobs.sh
+++ split_jobs.sh
@@ -6,6 +6,7 @@ np=$3 # number of processors per job
 ncore=$4 # number of cores per node
 jobname=$5
 queue=$6
+machine=$7
 
 i=1;
 j=1;
@@ -19,11 +20,11 @@ while read line; do
 	else
 		mkdir job\_$i
 		mv $curr_file job\_$i/task$i
-		cp /home/umai/my_gits/myTools/jobscr_template.comet job\_$i/jobscr_$i.comet
-		sed -i "s/#####/$jobname/g" job\_$i/jobscr_$i.comet
-		sed -i "s/%%%%%/$queue/g" job\_$i/jobscr_$i.comet
-		sed -i "s/@@@@@/$ncore/g" job\_$i/jobscr_$i.comet
-		echo python /home/umai/my_gits/myTools/core_launcher.py task$i $np >> job\_$i/jobscr_$i.comet
+		cp /home/umai/my_gits/myTools/jobscr_template.$machine job\_$i/jobscr_$i.$machine
+		sed -i "s/#####/$jobname/g" job\_$i/jobscr_$i.$machine
+		sed -i "s/%%%%%/$queue/g" job\_$i/jobscr_$i.$machine
+		sed -i "s/@@@@@/$ncore/g" job\_$i/jobscr_$i.$machine
+		echo python /home/umai/my_gits/myTools/core_launcher.py task$i $np >> job\_$i/jobscr_$i.$machine
 		j=1
 		i=$((i+1))
 		curr_file=`mktemp task-XXXXX`
@@ -33,11 +34,11 @@ done < $taskfile
 if [ -s $curr_file ]; then
 	mkdir job\_$i
 	mv $curr_file job\_$i/task$i
-	cp /home/umai/my_gits/myTools/jobscr_template.comet job\_$i/jobscr_$i.comet
-	sed -i "s/#####/$jobname/g" job\_$i/jobscr_$i.comet
-	sed -i "s/%%%%%/$queue/g" job\_$i/jobscr_$i.comet
-	sed -i "s/*****/$ncore/g" job\_$i/jobscr_$i.comet
-	echo python /home/umai/my_gits/myTools/core_launcher.py task$i $np >> job\_$i/jobscr_$i.comet
+	cp /home/umai/my_gits/myTools/jobscr_template.$machine job\_$i/jobscr_$i.$machine
+	sed -i "s/#####/$jobname/g" job\_$i/jobscr_$i.$machine
+	sed -i "s/%%%%%/$queue/g" job\_$i/jobscr_$i.$machine
+	sed -i "s/*****/$ncore/g" job\_$i/jobscr_$i.$machine
+	echo python /home/umai/my_gits/myTools/core_launcher.py task$i $np >> job\_$i/jobscr_$i.$machine
 else
 	rm $curr_file
 fi
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,4 +1,9 @@
 from=$1
 to=$2
-for i in `seq $from $to`; do cd job_$i; sbatch jobscr_$i.comet; cd ..; done
+machine=$3
 
+if [ $machine == 'comet' ]; then 
+	for i in `seq $from $to`; do cd job_$i; sbatch jobscr_$i.comet; cd ..; done
+elif [ $machine == 'tscc' ]; then
+	for i in `seq $from $to`; do cd job_$i; qsub jobscr_$i.tscc; cd ..; done
+fi
