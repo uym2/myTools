a3f32551f13555ebe0eb39040b6d0901e9ce3079
uym2 <ceciliamaitouyen@gmail.com>
2018-05-11 14:24:17 -0700

    new script to sample from a tree with specified number of ingroups and outgroups

--- sample_with_outgroups.py
+++ sample_with_outgroups.py
@@ -3,13 +3,31 @@
 # sample a tree with specified number of ingroups and outgroups
 
 from tree_lib import sample_with_outgroups
-from sys import argv
+from sys import argv, stdout
 from dendropy import Tree
+import argparse
+from os.path import splitext
 
-treefile = argv[1]
-n_ingroups = int(argv[2])
-n_outgroups = int(argv[3])
-n_reps = int(argv[4])
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-t","--tree",required=True,help="Input tree")
+parser.add_argument("-i","--ingroups",required=True,help="Number of ingroup species")
+parser.add_argument("-o","--outgroups",required=False,help="Number of outgroup species. Default:1")
+parser.add_argument("-r","--replicates",required=False,help="Number of replicates. Default: 1")
+parser.add_argument("-e","--outtree",required=False,help="Where to write the output trees to. Default: Inferred from the name of the input tree")
+parser.add_argument("-f","--outfile",required=False,help="Where to write the ingroups/outgroups of the samples to. Default: print to screen")
+
+
+args = vars(parser.parse_args())
+
+treefile = args["tree"]
+n_ingroups = int(args["ingroups"])
+n_outgroups = int(args["outgroups"]) if args["outgroups"] else 1
+n_reps = int(args["replicates"]) if args["replicates"] else 1
+
+base_name,ext = splitext(treefile)
+outtreeFile = args["outtree"] if args["outtree"] else (base_name + "_sampled.trees")
+outfile = args["outfile"]
 
 tree = Tree.get_from_path(treefile,'newick')
 
@@ -18,8 +36,22 @@ check,samples = sample_with_outgroups(tree, n_ingroups, n_outgroups=n_outgroups,
 if not check:
     print("The tree is not large enough for this sampling size!")
 else:
-    for t,igs,ogs in samples:
-        #print(igs,ogs)
-        print(t.as_string('newick'))
+    if outfile:
+        fout_info = open(outfile,'w')
+    else:
+        fout_info = stdout
 
+    fout_info.write("Sampled " + str(n_reps) + " replicate(s) of " + str(n_ingroups) + " ingroup(s) and " + str(n_outgroups) + " outgroup(s)\n")
+    with open(outtreeFile,'w') as fout_tree:
+        for i,(t,igs,ogs) in enumerate(samples):
+            fout_info.write("Rep " + str(i+1)+":")
+            fout_info.write(" Ingroups: ")
+            for i in igs:
+                fout_info.write(i + " ")
+            fout_info.write("Outgroups: ")
+            for o in ogs:
+                fout_info.write(o + " ")
+            fout_info.write("\n")
+            fout_tree.write(t.as_string('newick'))
 
+    fout_info.close()
